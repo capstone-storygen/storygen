45bcde85b26f5f082f04efaa4989777408f52d91
Amanjeet Singh Barar <84509893+amanbarar@users.noreply.github.com>
2024-02-27 10:31:02 +0530

    Setup basic backend (#10)
    * add: scripts for dev and start
    
    * add: route for story generation logic
    
    * add: POST request for API content
    
    * add: storyRoutes in index.js
    
    * fix: response data of POST req stroyRoutes

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,15 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 
+const storyRoutes = require("./routes/storyRoutes");
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 dotenv.config();
+
+app.use("/api/", storyRoutes);
 
 port = process.env.port || 5000;
 
---
+++ backend/routes/storyRoutes.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const router = express.Router();
+
+const { Configuration, OpenAIApi, ConflictError } = require("openai");
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAiApi(configuration);
+
+router.post("/story", async (req, res) => {
+    const { prompt } = req.body;
+
+    try {
+        const response = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {
+                    role: "assistant", // refer OpenAI documentation in order to understand the concept of role
+                    content: "",
+                },
+            ],
+            temperature: 1,
+            max_tokens: 256,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+        });
+        res.send(response.data.choices[0].message.content);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+module.exports = router;
