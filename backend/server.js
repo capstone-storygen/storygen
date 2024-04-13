d34ef1e8e738c6a22ff258a78ab1b1f084bc0163
Aman Barar <barar.amanjeet.aman@gmail.com>
2024-04-13 20:31:41 +0530

    add: corsOption and errorMiddleware

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,32 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const dotenv = require("dotenv");
-
-const storyRoute = require("./routes/storyRoute");
+require("dotenv").config();
 
 const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+const storyRoute = require("./routes/storyRoute");
+const { errorMiddleware } = require("./middlewares/errorMiddleware");
+const FRONTEND = process.env.FRONTEND;
 
-dotenv.config();
+app.use(bodyParser.json());
+var corsOptions = {
+    origin: FRONTEND,
+    optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+// using this route for testing errorMiddleware
+app.get("/", (req, res) => {
+    // throw new Error("test error");
+    // res.send("Hello server");
+});
 
 app.use("/api/story", storyRoute);
 
-port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+app.use(errorMiddleware);
