4135387607e324948db6913b157ed6f4cef1e0f6
Aman Barar <barar.amanjeet.aman@gmail.com>
2024-04-15 12:51:11 +0530

    add: docker-compose.yaml

--- frontend/src/Utils/storyApi.js
+++ frontend/src/Utils/storyApi.js
@@ -1,5 +1,17 @@
 async function generateStory(prompt) {
-    const response = await fetch("/api/story", {
+    let apiUrl = "";
+
+    if (process.env.DOCKER_ENV === "true") {
+        apiUrl = `${
+            process.env.BACKEND_URL || "http://backend:5000"
+        }/api/story`;
+    } else {
+        apiUrl = `${
+            process.env.REACT_APP_BACKEND_URL || "http://localhost:5000"
+        }/api/story`;
+    }
+
+    const response = await fetch(apiUrl, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -7,6 +19,7 @@ async function generateStory(prompt) {
         body: JSON.stringify({ prompt }),
     });
     if (!response.ok) {
+        console.log("API URL:", apiUrl);
         throw new Error("Network response was not ok");
     }
     const data = await response.text();
